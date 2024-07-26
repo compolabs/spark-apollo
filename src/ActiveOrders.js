d4a4e0a4195498e9eadde10a766303b56e012676
Røbėrtø <106074508+EchoDex@users.noreply.github.com>
2024-07-26 16:14:50 +0400

    feat: checking query and subscription

--- src/ActiveOrders.js
+++ src/ActiveOrders.js
@@ -1,44 +1,138 @@
-import React from "react"
-import { useSubscription, gql } from "@apollo/client"
+import React, { useEffect, useState } from "react"
+import { useSubscription, gql, useQuery } from "@apollo/client"
 
 const ACTIVE_ORDERS_SUBSCRIPTION = gql`
-  subscription {
-    ActiveOrders {
+  subscription (
+    $limit: Int!
+    $where: Order_bool_exp
+    $priceOrder: order_by!
+  ) {
+    Order(limit: $limit, where: $where, order_by: { price: $priceOrder }) {
       id
-      orders {
-        id
-        user
-        timestamp
-        order_type
-        amount
-        asset
-        price
-        activeOrderRef_id
-      }
+      asset
+      asset_type
+      amount
+      initial_amount
+      order_type
+      price
+      status
+      user
+      timestamp
     }
   }
 `
 
+const ACTIVE_ORDERS_QUERY = gql`
+  query (
+    $limit: Int!
+    $where: Order_bool_exp
+    $priceOrder: order_by!
+  ) {
+    Order(limit: $limit, where: $where, order_by: { price: $priceOrder }) {
+      id
+      asset
+      asset_type
+      amount
+      initial_amount
+      order_type
+      price
+      status
+      user
+      timestamp
+    }
+  }
+`
+
+const handleOnComplete = (setter) => {
+  setter(Date.now() - CURRENT_TIME);
+}
+
+const CURRENT_TIME = Date.now();
+
 function ActiveOrders() {
-  const { data, loading, error } = useSubscription(ACTIVE_ORDERS_SUBSCRIPTION)
+  const [sellSubscriptionTime, setSellSubscriptionTime] = useState(null);
+  const [buySubscriptionTime, setBuySubscriptionTime] = useState(null);
+  const [sellQueryTime, setSellQueryTime] = useState(null);
+  const [buyQueryTime, setBuyQueryTime] = useState(null);
 
-  console.log("Subscription Data:", data)
+  const { data: sellData } = useSubscription(ACTIVE_ORDERS_SUBSCRIPTION, {
+    variables: {
+      limit: 50,
+      priceOrder: "asc",
+      where: {
+        asset: {_eq: "0xccceae45a7c23dcd4024f4083e959a0686a191694e76fa4fb76c449361ca01f7"},
+        order_type: {_eq: "Sell"},
+        status: {_eq: "Active"}
+      },
+    }
+  })
+  const { data: buyData } = useSubscription(ACTIVE_ORDERS_SUBSCRIPTION, {
+    variables: {
+      limit: 50,
+      priceOrder: "desc",
+      where: {
+        asset: {_eq: "0xccceae45a7c23dcd4024f4083e959a0686a191694e76fa4fb76c449361ca01f7"},
+        order_type: {_eq: "Buy"},
+        status: {_eq: "Active"}
+      },
+    }
+  })
 
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
+  const { data: sellDataQuery } = useQuery(ACTIVE_ORDERS_QUERY, {
+    variables: {
+      limit: 50,
+      priceOrder: "asc",
+      where: {
+        asset: {_eq: "0xccceae45a7c23dcd4024f4083e959a0686a191694e76fa4fb76c449361ca01f7"},
+        order_type: {_eq: "Sell"},
+        status: {_eq: "Active"}
+      },
+    }
+  })
+  const { data: buyDataQuery } = useQuery(ACTIVE_ORDERS_QUERY, {
+    variables: {
+      limit: 50,
+      priceOrder: "desc",
+      where: {
+        asset: {_eq: "0xccceae45a7c23dcd4024f4083e959a0686a191694e76fa4fb76c449361ca01f7"},
+        order_type: {_eq: "Buy"},
+        status: {_eq: "Active"}
+      },
+    }
+  })
+
+  useEffect(() => {
+    if (sellSubscriptionTime) return;
+    sellData && handleOnComplete(setSellSubscriptionTime);
+  }, [sellData, sellSubscriptionTime])
+
+  useEffect(() => {
+    if (buySubscriptionTime) return;
+    buyData && handleOnComplete(setBuySubscriptionTime);
+  }, [buyData, buySubscriptionTime])
+
+  useEffect(() => {
+    if (sellQueryTime) return;
+    sellDataQuery && handleOnComplete(setSellQueryTime);
+  }, [sellDataQuery, sellQueryTime])
+
+  useEffect(() => {
+    if (sellQueryTime) return;
+    buyDataQuery && handleOnComplete(setBuyQueryTime);
+  }, [buyDataQuery, sellQueryTime])
+
+  console.log("Subscription Sell Data:", sellData)
+  console.log("Subscription Buy Data:", buyData)
+
+  console.log("Query Sell Data:", sellDataQuery)
+  console.log("Query Buy Data:", buyDataQuery)
 
   return (
     <div>
-      {/* {data.activeOrders.orders.map((order) => (
-        <div key={order.id}>
-          <p>User: {order.user}</p>
-          <p>Timestamp: {new Date(order.timestamp).toLocaleString()}</p>
-          <p>Order Type: {order.order_type}</p>
-          <p>Amount: {order.amount}</p>
-          <p>Asset: {order.asset}</p>
-          <p>Price: {order.price}</p>
-        </div>
-      ))} */}
+      <p>Subscription Sell Time: {sellSubscriptionTime} ms</p>
+      <p>Subscription Buy Time: {buySubscriptionTime} ms</p>
+      <p>Query Sell Time: {sellQueryTime} ms</p>
+      <p>Query Buy Time: {buyQueryTime} ms</p>
     </div>
   )
 }
--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,16 +1,41 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions"
 import { createClient } from "graphql-ws"
 
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from "@apollo/client";
+
+import { getMainDefinition } from "@apollo/client/utilities";
+
 const wsLink = new GraphQLWsLink(
   createClient({
-    url: "ws://localhost:8080/v1/graphql",
+    url: "wss://indexer.bigdevenergy.link/67b693c/v1/graphql",
   })
 )
 
+const httpLink = new HttpLink({
+  uri: "https://indexer.bigdevenergy.link/67b693c/v1/graphql",
+});
+
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
 const client = new ApolloClient({
-  link: wsLink,
+  link: splitLink,
   cache: new InMemoryCache(),
-})
+});
 
 export default client
